5abdbd0492264b8c72c3a5bd42c931e137a7f1d8
Renne Rocha <renne@rennerocha.com>
2019-10-17 12:11:50 -0300

    Pipeline to extract only desired info

--- imoveis/imoveis/pipelines.py
+++ imoveis/imoveis/pipelines.py
@@ -1,11 +1,26 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from urllib.parse import urlparse
+from scrapy.exceptions import DropItem
 
 
 class ImoveisPipeline(object):
     def process_item(self, item, spider):
-        return item
+        if not item["comercializacao"]["locacao"]["ativa"]:
+            raise DropItem("Not for rent")
+
+        property_code = item["sigla"]
+        parsed_url = urlparse(spider.busca_url)
+        property_url = (
+            f"{parsed_url.scheme}://{parsed_url.netloc}/imovel/{property_code}"
+        )
+
+        return {
+            "bairro": item["local"]["bairro"],
+            "latitude": item["local"]["coordenadas"][0],
+            "longitude": item["local"]["coordenadas"][1],
+            "preco": item["comercializacao"]["locacao"]["preco"],
+            "preco_total": item["comercializacao"]["locacao"]["total"],
+            "area_construida": item["numeros"]["areas"]["construida"],
+            "url": property_url,
+            "imobiliaria": spider.real_estate,
+            "codigo": item["sigla"],
+        }
