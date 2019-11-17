91a82f4c2aee5bf6cb8e6f4d143c12cb209a9b3f
Renne Rocha <renne@rennerocha.com>
2019-11-17 12:10:38 -0300

    Filter properties with same code

--- imoveis/imoveis/pipelines.py
+++ imoveis/imoveis/pipelines.py
@@ -3,7 +3,20 @@ from urllib.parse import urlparse
 from scrapy.exceptions import DropItem
 
 
-class ImoveisPipeline(object):
+class ImoveisPipeline:
     def process_item(self, item, spider):
         item["date"] = datetime.date.today()
         return item
+
+
+class FilterDuplicatedPropertiesPipeline:
+    def __init__(self):
+        self.visited_codes = set()
+
+    def process_item(self, item, spider):
+        item_code = item["code"]
+        if item_code in self.visited_codes:
+            raise DropItem(f"Already collected {item_code}")
+        else:
+            self.visited_codes.add(item["code"])
+            return item
